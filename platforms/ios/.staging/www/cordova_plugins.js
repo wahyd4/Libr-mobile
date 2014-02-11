694d8eb2540fbf066a1478a1fb824b94fa49b4ed
junv <wahyd4@gmail.com>
2014-02-11 16:17:34 +0800

    add new icon

--- platforms/ios/.staging/www/cordova_plugins.js
+++ platforms/ios/.staging/www/cordova_plugins.js
@@ -1,6 +1,13 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
     {
+        "file": "plugins/com.phonegap.plugins.barcodescanner/www/barcodescanner.js",
+        "id": "com.phonegap.plugins.barcodescanner.BarcodeScanner",
+        "clobbers": [
+            "cordova.plugins.barcodeScanner"
+        ]
+    },
+    {
         "file": "plugins/org.apache.cordova.device/www/device.js",
         "id": "org.apache.cordova.device.device",
         "clobbers": [
@@ -34,21 +41,14 @@ module.exports = [
         "clobbers": [
             "navigator.geolocation"
         ]
-    },
-    {
-        "file": "plugins/com.phonegap.plugins.barcodescanner/www/barcodescanner.js",
-        "id": "com.phonegap.plugins.barcodescanner.BarcodeScanner",
-        "clobbers": [
-            "cordova.plugins.barcodeScanner"
-        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
+    "com.phonegap.plugins.barcodescanner": "1.0.1",
     "org.apache.cordova.device": "0.2.7",
-    "org.apache.cordova.geolocation": "0.3.5",
-    "com.phonegap.plugins.barcodescanner": "1.0.1"
+    "org.apache.cordova.geolocation": "0.3.5"
 }
 // BOTTOM OF METADATA
 });
